fde46eb2975b5fd3aeaca2e131b46b2bbf1aedf5
HP <HP@DESKTOP-L89B3LR>
2021-02-06 01:32:20 +0300

    First commit

---
+++ FirstPracticaHomewor/src/fristpracticahomewor.java
@@ -0,0 +1,11 @@
+// This program is written by Sultan Alyami 
+// Date : Feb 6,2021
+// Course Name: Object-oriented programming 
+public class fristpracticahomewor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("X Y");
+	}
+
+}
